24cbc9a9636107cc9bc3409ee77934cbdb348009
matt1913 <45541746+matt1913@users.noreply.github.com>
2019-11-22 11:55:16 -0500

    Update 101ATermProject.py

--- 101ATermProject.py
+++ 101ATermProject.py
@@ -3,6 +3,15 @@
 """
 Created on Thu Nov 21 16:20:18 2019
 
-@author: andrew
+@authors: andrew, matt
 """
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import lightgbm as lgbm
+from sklear.preprocessing import LabelEncoder
+from sklearn.model_selection import KFold
+import dateimte
+import gc
+
+DATA_SET = 
