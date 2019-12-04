d1283d9ad5a5a311a50a6fcc901324e323895598
BubbleShadow <yifei.han@uconn.edu>
2019-12-03 22:57:20 -0500

    bug fixes

--- 101ATermProject.py
+++ 101ATermProject.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import pandas as pd
 import numpy as np
@@ -7,9 +6,8 @@ import numpy as np
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
 from pandas.api.types import is_categorical_dtype
 
-PATH = "../input/ashrae-energy-prediction/"
-
-df_train = pd.read_csv(PATH + 'train.csv')
+PATH = '/Users/jacobhan/Documents/GitHub/101TermProject'
+df_train = pd.read_csv(PATH + '/ashrae-energy-prediction/weather_train.csv')
 
 def reduce_mem(df, use_float16 = False):
 
@@ -17,9 +15,8 @@ def reduce_mem(df, use_float16 = False):
 	print("initial df mem usage: {:.2f}mb".format(initial_mem))
   
 	for col in df.columns:
-		if (is_datetime(df[col] or is_categorical_dtype(df[col]))):
+		if is_datetime(df[col]) or is_categorical_dtype(df[col]):
 			continue
-
 		col_dtype = df[col].dtype
 					   
 		if col_dtype != object:
@@ -38,12 +35,14 @@ def reduce_mem(df, use_float16 = False):
 			else:
 				if use_float16 and col_min > np.iinfo(np.float16) and col_max < np.iinfo(np.col16):
 					df[col] = df[col].astype(np.float16)
-				elif col_min > np.iinfo(np.float32) and col_max < np.iinfo(np.col32):
+				elif col_min > np.finfo(np.float32).min and col_max < np.finfo(np.float32).max:
 					df[col] = df[col].astype(np.float32)
 				else:
-					df[col] = df[col].astype(float64)
+					df[col] = df[col].astype(np.float64)
 		else:
 			df[col] = df[col].astype("category")
-	
+
 	final_mem = df.memory_usage().sum() / 1024 ** 2
 	print("reduced df mem usage: {:.2f}mb, reduced by {:.1f} percent".format(final_mem, (100 * (initial_mem - final_mem) / initial_mem)))	
+
+reduce_mem(df_train, False)
